2a2df767d75865fbb7344c26eaa9cf59d47d4818
Alex <aguo11@my.bcit.ca>
2020-12-09 20:41:21 -0800

    Finished Javascript. HTML and CSS were changed slightly in the process. Final submission.

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+const thumbBar = document.querySelector('#thumb-bar')
+const displayedImage = document.querySelector('.displayed-img')
+const effectSection = document.querySelector('.effects')
+const submitButton = document.querySelector('button')
+
+
 function addImages() {
     for (i = 1; i <= 5; i++) {
         var image = document.createElement("img");
         image.setAttribute("src", "images/pic" + i + ".jpg");
         document.querySelector('#thumb-bar').appendChild(image);
-        console.log("image added")
+        console.log("images added")
     }
 }
 
-
 addImages()
+
+
+thumbBar.addEventListener("mouseover", mouseOverObject => {
+    effectSection.classList.add("effects_visible")
+    newImage = mouseOverObject.target.src
+    displayedImage.src = newImage
+    console.log(newImage)
+})
+
+submitButton.addEventListener("click", () => {
+    const userInput = document.querySelector('#effect').value
+    var imageToBlur = displayedImage
+    if (userInput == 'blur') {
+        imageToBlur.src = imageToBlur.src.replace('.', 'b.')
+    } else {
+        document.querySelector('#effect').value = "Invalid input! Try 'blur'"
+    }
+})
